61a4c92bbdd4eebfc7fc85679cfac281c90fde35
Quinton Aiken <quintonlaiken@gmail.com>
2015-06-03 13:22:42 -0500

    add findLargestPrime function

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -100,26 +100,37 @@ describe("About Applying What We Have Learnt", function() {
 
   /*********************************************************************************/
   /* UNCOMMENT FOR EXTRA CREDIT */
-  /*
-  it("should find the largest prime factor of a composite number", function () {
   
+  it("should find the largest prime factor of a composite number", function () {
+
+    var findLargestPrime = function(num) {
+      var i = num;
+
+      while(--i) {
+        if( num % i === 0 && i % 2 !== 0 ) return i;
+      }
+
+    };
+
+    expect(findLargestPrime(485)).toBe(97);
+    expect(findLargestPrime(9747)).toBe(3249);
+
   });
 
   it("should find the largest palindrome made from the product of two 3 digit numbers", function () {
-    
   });
 
-  it("should find the smallest number divisible by each of the numbers 1 to 20", function () {
+  // it("should find the smallest number divisible by each of the numbers 1 to 20", function () {
       
     
-  });
+  // });
 
-  it("should find the difference between the sum of the squares and the square of the sums", function () {
+  // it("should find the difference between the sum of the squares and the square of the sums", function () {
     
-  });
+  // });
 
-  it("should find the 10001st prime", function () {
+  // it("should find the 10001st prime", function () {
 
-  });
-  */
+  // });
+  
 });
